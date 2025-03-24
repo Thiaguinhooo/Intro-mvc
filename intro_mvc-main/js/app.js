4a1dd5fb82c933debb718cb454c379d1ee37f2e9
THIAGO RIBEIRO GODOY DE SOUZA <CG3034631@car.ifsp>
2025-03-24 15:45:45 -0300

    criando a view

--- intro_mvc-main/js/app.js
+++ intro_mvc-main/js/app.js
@@ -1,22 +1,31 @@
+const alunos = [
+    {
+      _id: 0,
+      nome: "chico melato",
+      notas: {
+        backend_1: [7, 8.5, 9, 6.5],
+        frontend_2: [2, 2, 2, 2],
+        bancodados: [2, 2, 3, 3],
+        ferramentas: [3, 3, 3, 3],
+      },
+    },
+    {
+      _id: 1,
+      nome: "talita lima",
+      notas: {
+        backend_1: [4, 4, 4, 4],
+        frontend_2: [4, 4, 5, 5],
+        bancodados: [5, 5, 6, 6],
+        ferramentas: [7, 7, 8, 9],
+      },
+    },
+  ];
+
+  const alunoService = new AlunoService()
+
 alunos.forEach(aluno => {
-    aluno.media = {}
-    for(let materia in aluno.notas) {
-        aluno.media[materia] = average(...aluno.notas[materia])
-    }
+    alunoService.add(new AlunoModel(aluno))
 })
 
-console.log(alunos)
-
-const htmlHeader = document.createElement('tr')
-htmlHeader.innerHTML = '<td>Nome</td>'
-
-const htmlHeaderMaterias = Object.keys(alunos[0].notas).map(materia => {
-    console.log(materia)
-    return `<td>${materia}</td>`
-}).join('')
-
-console.log(htmlHeaderMaterias)
-htmlHeader.innerHTML += htmlHeaderMaterias
-console.log(htmlHeader.innerHTML)
-
-document.querySelector('[data-table-alunos] thead').appendChild(htmlHeader)
+const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
+const alunoController = new AlunoController(alunoService, alunoView)
